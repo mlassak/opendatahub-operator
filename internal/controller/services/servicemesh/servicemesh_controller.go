5035f9eba229d8db9a6f73b20c51644fb268ff59
Marek Lassak <mlassak@redhat.com>
2025-05-14 12:19:12 +0200

    feat: add dedicated ServiceMesh CRD, adjust DSCI and SM controllers accordingly

--- internal/controller/services/servicemesh/servicemesh_controller.go
+++ internal/controller/services/servicemesh/servicemesh_controller.go
@@ -2,47 +2,19 @@ package servicemesh
 
 import (
 	"context"
-	"reflect"
 
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/event"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
-	dsciv1 "github.com/opendatahub-io/opendatahub-operator/v2/api/dscinitialization/v1"
+	serviceApi "github.com/opendatahub-io/opendatahub-operator/v2/api/services/v1alpha1"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster"
 )
 
 type ServiceMeshReconciler struct {
 	Client   client.Client
 	Recorder record.EventRecorder
-}
-
-func (r *ServiceMeshReconciler) dsciServiceMeshPredicate() predicate.Funcs {
-	return predicate.Funcs{
-		UpdateFunc: func(e event.UpdateEvent) bool {
-			oldDsci, ok := e.ObjectOld.(*dsciv1.DSCInitialization)
-			if !ok {
-				return false
-			}
-			newDsci, ok := e.ObjectNew.(*dsciv1.DSCInitialization)
-			if !ok {
-				return false
-			}
-
-			return !reflect.DeepEqual(oldDsci.Spec.ServiceMesh, newDsci.Spec.ServiceMesh)
-		},
-
-		CreateFunc: func(e event.CreateEvent) bool {
-			return true
-		},
-
-		DeleteFunc: func(e event.DeleteEvent) bool {
-			return true
-		},
-	}
 }
 
 // SetupWithManager sets up the controller with the Manager.
@@ -51,7 +23,7 @@ func (r *ServiceMeshReconciler) SetupWithManager(ctx context.Context, mgr ctrl.M
 
 	return ctrl.NewControllerManagedBy(mgr).
 		Named("servicemesh-controller").
-		For(&dsciv1.DSCInitialization{}, builder.WithPredicates(r.dsciServiceMeshPredicate())).
+		For(&serviceApi.ServiceMesh{}).
 		Complete(r)
 }
 
@@ -59,22 +31,27 @@ func (r *ServiceMeshReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	log := logf.FromContext(ctx)
 	log.Info("Reconciling ServiceMesh controller")
 
-	dsciInstance := &dsciv1.DSCInitialization{}
-	if err := r.Client.Get(ctx, req.NamespacedName, dsciInstance); err != nil {
+	sm := &serviceApi.ServiceMesh{}
+	if err := r.Client.Get(ctx, req.NamespacedName, sm); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	if !dsciInstance.DeletionTimestamp.IsZero() {
-		// DSCI is being deleted, remove ServiceMesh
-		if err := r.removeServiceMesh(ctx, dsciInstance); err != nil {
+	dsci, err := cluster.GetDSCI(ctx, r.Client)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if !sm.DeletionTimestamp.IsZero() {
+		// ServiceMesh instance is being deleted
+		if err := r.removeServiceMesh(ctx, dsci); err != nil {
 			return ctrl.Result{}, err
 		}
 
-		return ctrl.Result{}, nil
+		return ctrl.Result{}, err
 	}
 
 	// Apply Service Mesh configurations
-	if err := r.configureServiceMesh(ctx, dsciInstance); err != nil {
+	if err := r.configureServiceMesh(ctx, dsci); err != nil {
 		return ctrl.Result{}, err
 	}
 
